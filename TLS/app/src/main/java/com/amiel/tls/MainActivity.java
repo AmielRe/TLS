ddc214086a52847633c937dbee0e18bbd000521c
Emilio1999 <amieli1999@gmail.com>
2020-07-10 16:31:25 +0300

    Removed action bar setup. Added tab adapter for switching from girls to boys (for topaz and for lotem).

--- TLS/app/src/main/java/com/amiel/tls/MainActivity.java
+++ TLS/app/src/main/java/com/amiel/tls/MainActivity.java
@@ -2,13 +2,19 @@ package com.amiel.tls;
 
 import android.os.Bundle;
 
+import com.amiel.tls.ui.lotem.LotemBoysFragment;
+import com.amiel.tls.ui.lotem.LotemGirlsFragment;
+import com.amiel.tls.ui.topaz.TopazBoysFragment;
+import com.amiel.tls.ui.topaz.TopazGirlsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.ViewPager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
     }
 
