6962633427705b83213ab685502dceb2d98dac43
Emilio1999 <amieli1999@gmail.com>
2020-07-14 17:19:57 +0300

    Added custom spinner and spinner items to display available rooms for new persons.

---
+++ TLS/app/src/main/java/com/amiel/tls/CustomRoomSpinnerAdapter.java
@@ -0,0 +1,51 @@
+package com.amiel.tls;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.amiel.tls.db.entities.Room;
+
+public class CustomRoomSpinnerAdapter extends BaseAdapter {
+
+    private Context context;
+    private Room[] rooms;
+    private LayoutInflater inflter;
+
+    CustomRoomSpinnerAdapter(Context applicationContext, Room[] rooms) {
+        this.context = applicationContext;
+        this.rooms = rooms;
+        inflter = (LayoutInflater.from(applicationContext));
+    }
+
+    @Override
+    public int getCount() {
+        return rooms.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return rooms[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view = inflter.inflate(R.layout.custom_room_spinner_row, null);
+        TextView roomName = (TextView) view.findViewById(R.id.custom_room_spinner_row_room_name);
+        TextView roomCapacity = (TextView) view.findViewById(R.id.custom_room_spinner_row_capacity);
+        TextView roomGenderType = (TextView) view.findViewById(R.id.custom_room_spinner_row__gender_type);
+
+        roomName.setText(rooms[i].roomName);
+        roomCapacity.setText(String.format("%d/%d", rooms[i].currentCapacity, rooms[i].maxCapacity));
+        roomGenderType.setText(String.format("%s (%s): ", CommonUtils.intToRoomType(rooms[i].roomType), CommonUtils.intToGender(rooms[i].roomGender)));
+        return view;
+    }
+}
