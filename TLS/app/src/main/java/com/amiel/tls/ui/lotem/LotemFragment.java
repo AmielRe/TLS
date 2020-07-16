5fda564c68c1db0dcbd9edbc9b65bdcaa9a790a3
Emilio1999 <amieli1999@gmail.com>
2020-07-16 20:54:18 +0300

    Fixed a bug cause by the tablayout and the viewpager overlaping and thus the tabs could not be clicked.

--- TLS/app/src/main/java/com/amiel/tls/ui/lotem/LotemFragment.java
+++ TLS/app/src/main/java/com/amiel/tls/ui/lotem/LotemFragment.java
@@ -1,43 +1,17 @@
 package com.amiel.tls.ui.lotem;
 
-import android.annotation.SuppressLint;
-import android.app.DatePickerDialog;
-import android.content.DialogInterface;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.DatePicker;
-import android.widget.ListView;
-import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.widget.AppCompatSpinner;
-import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import com.amiel.tls.R;
-import com.amiel.tls.RoomsListAdapter;
 import com.amiel.tls.TabAdapter;
-import com.amiel.tls.db.DBHandler;
-import com.amiel.tls.db.entities.Person;
-import com.amiel.tls.db.entities.Room;
-import com.amiel.tls.CustomRoomSpinnerAdapter;
-import com.github.clans.fab.FloatingActionButton;
-import com.github.clans.fab.FloatingActionMenu;
 import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.firebase.database.DatabaseError;
-
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
 public class LotemFragment extends Fragment {
 
