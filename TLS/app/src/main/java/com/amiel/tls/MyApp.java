985ace97b26c1a7fcd25fe62353fc3fdc4b6fae0
AmielRe <amieli1999@gmail.com>
2020-10-10 15:41:27 +0300

    Add app class
    Added app class to use the firebase methods before any other methods are called.
    This cause crash sometimes and with this way, the crash is avoided.

---
+++ TLS/app/src/main/java/com/amiel/tls/MyApp.java
@@ -0,0 +1,15 @@
+package com.amiel.tls;
+
+import android.app.Application;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        FirebaseDatabase.getInstance().getReference().keepSynced(true);
+    }
+}
